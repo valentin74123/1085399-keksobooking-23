272a9cd1c2d129052da713ecd3e9cc7e4bb73228
valentin74123 <58230964+valentin74123@users.noreply.github.com>
2021-06-25 22:58:38 +0500

    Добавляет отрисовку карточек

---
+++ js/card.js
@@ -0,0 +1,43 @@
+import {createOffers} from './data.js';
+
+const cardOffersElement = document.querySelector('#map-canvas');
+const cardOffersTemplate = document.querySelector('#card').content.querySelector('.popup');
+
+const cardOffers = createOffers();
+
+cardOffers.forEach(({author: {avatar}, offer: {title, address, price, type, rooms, guests, checkin, checkout, features, description, photos}}) => {
+  const offerElement = cardOffersTemplate.cloneNode(true);
+
+  offerElement.querySelector('.popup__title').textContent = title;
+  offerElement.querySelector('.popup__text--address').textContent = address;
+  offerElement.querySelector('.popup__text--price').textContent = `${price} ₽/ночь`;
+  offerElement.querySelector('.popup__type').textContent = type;
+  offerElement.querySelector('.popup__text--capacity').textContent = `${rooms} комнаты для ${guests} гостей`;
+  offerElement.querySelector('.popup__text--time').textContent = `Заезд после ${checkin}, выезд до ${checkout}`;
+
+  const featuresListElement = offerElement.querySelector('.popup__features');
+  const modifiers = features.map((feature) => `popup__feature--${feature}`);
+  featuresListElement.querySelectorAll('.popup__feature').forEach((item) => {
+    const modifier = item.classList[1];
+
+    if(!modifiers.includes(modifier)) {
+      item.remove();
+    }
+  });
+
+  offerElement.querySelector('.popup__description').textContent = description;
+
+  const photosListElement = offerElement.querySelector('.popup__photos');
+  const photoElement = photosListElement.querySelector('.popup__photo');
+  photos.map((src) => {
+    const photo = photoElement.cloneNode(true);
+    photo.src = src;
+    photosListElement.appendChild(photo);
+  });
+  photoElement.parentNode.removeChild(photoElement);
+
+
+  offerElement.querySelector('.popup__avatar').src = avatar;
+
+  cardOffersElement.appendChild(offerElement);
+});
